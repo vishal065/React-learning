a559a28c3e7458987ca513ca7c2c6553b2691744
vishal <dasvishal60@gmail.com>
2024-04-15 06:05:50 +0530

    setting up auth account services for backend

---
+++ MegaBlog-Project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL); //access the env variable
+  return (
+    <>
+      <h1>dewjaiwjd</h1>
+    </>
+  );
+}
+
+export default App;
